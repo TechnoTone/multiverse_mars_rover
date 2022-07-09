850f483a009be499ca2cb2c57f26dfdf4f5e601f
Tony Hunt <GitHub.TonyHunt@spamgourmet.com>
2022-07-09 15:18:49 +0100

    Parse rover inputs

--- rover.js
+++ rover.js
@@ -1,10 +1,29 @@
 module.exports.parseInput = function (input) {
   const [gridInput, ...roverInputs] = input.split("\n");
   const grid = parseGrid(gridInput);
-  return { grid };
+  const rovers = parseRovers(roverInputs);
+  return { grid, rovers };
 };
 
 function parseGrid(input) {
   const values = input.split(" ").map(Number);
   return { width: values[0], height: values[1] };
+}
+
+function parseRovers(inputs) {
+  return inputs.map(parseRover);
+}
+
+function parseRover(input) {
+  console.log({ input });
+  partsRegex = /^\((\d+), (\d+), ([NESW])\) ([FLR]*)/;
+  const parts = partsRegex.exec(input);
+  const [x, y] = parts.slice(1, 3).map(Number);
+  const [direction, instructions] = parts.slice(3);
+  return {
+    position: { x, y },
+    direction,
+    instructions,
+    lost: false,
+  };
 }
--- rover.test.js
+++ rover.test.js
@@ -1,10 +1,10 @@
 const { describe, test } = require("@jest/globals");
+const { expect } = require("expect");
 const { parseInput } = require("./rover");
 
 const TEST_INPUT_1 = `4 8
 (2, 3, E) LFRFF
-(0, 2, N) FFLFRFF
-`;
+(0, 2, N) FFLFRFF`;
 
 describe("parses input correctly", () => {
   const result = parseInput(TEST_INPUT_1);
@@ -12,5 +12,25 @@ describe("parses input correctly", () => {
   test("parses grid dimensions", () => {
     expect(result.grid.width).toBe(4);
     expect(result.grid.height).toBe(8);
+  });
+
+  test("parses rover inputs", () => {
+    expect(result.rovers).toHaveLength(2);
+    expect(result.rovers[0]).toMatchObject({
+      position: { x: 2, y: 3 },
+      direction: "E",
+      instructions: "LFRFF",
+    });
+    expect(result.rovers[1]).toMatchObject({
+      position: { x: 0, y: 2 },
+      direction: "N",
+      instructions: "FFLFRFF",
+    });
+  });
+
+  test("all rovers start as not lost", () => {
+    result.rovers.forEach((rover) => {
+      expect(rover.lost).toBeFalsy();
+    });
   });
 });
